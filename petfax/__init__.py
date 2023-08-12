9dcca9b708c2dae646a3139a3203c21ec8a60b74
vicentemorales <vmorales121@gmail.com>
2023-08-12 11:28:43 -0700

    added facts/new and show pages

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -3,12 +3,18 @@ from flask import Flask
 def create_app(): 
     app = Flask(__name__)
 
+    # index route
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
-    #register pet blueprint
-    from . import pet
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
     
     return app
--- petfax/pet.py
+++ petfax/pet.py
@@ -8,3 +8,9 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 @bp.route('/')
 def index(): 
     return render_template('index.html', pets=pets)
+
+#renders show page
+@bp.route('/<int:id>')
+def show(id):
+    pet = pets[id-1]
+    return render_template('pets/show.html', pet=pet)
